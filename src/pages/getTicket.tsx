e70adebacf99515ae225d99f8158373849876bdf
satendra dhakar <satendra.dhakar@qtsolv.com>
2024-06-14 12:56:18 +0530

    design changes

--- src/pages/getTicket.tsx
+++ src/pages/getTicket.tsx
@@ -82,7 +82,23 @@ const GetRequests: React.FC = () => {
                   <IonLabel>No requests found</IonLabel>
                 )}
 
-            </IonCard>
+           
+
+            <div className='pagination'>
+            <ul id="border-pagination">
+            <li><a className="" href="#">«</a></li>
+            <li><a href="#">1</a></li>
+            <li><a href="#" className="active">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">6</a></li>
+            <li><a href="#">»</a></li>
+             </ul> 
+             </div>
+
+             </IonCard>
+
             <div className='footer'>
               <IonTitle className='footer ion-text-center'>Helpline | +91 90999 XXXXX</IonTitle>
             </div>
