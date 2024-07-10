e26034b1b227a7d62b03ab5460bfd6dab7a953ce
Sheikh Wasim <sheikhwasim009@gmail.com>
2024-07-10 23:35:02 +0530

    guard page updated

--- src/pages/getTicket.tsx
+++ src/pages/getTicket.tsx
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { IonButtons, IonLoading, IonContent, IonGrid, IonRow, IonCol, IonHeader, IonLabel, IonMenuButton, IonPage, IonTitle, IonToolbar, IonImg, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonFab, IonFabButton, IonIcon, IonItem, IonList, IonInput, IonSelectOption, IonButton, IonModal, IonSelect, useIonToast } from '@ionic/react';
+import { IonButtons, IonLoading, IonContent, IonGrid, IonRow, IonCol, IonHeader, IonLabel, IonMenuButton, IonPage, IonTitle, IonToolbar, IonImg, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonFab, IonFabButton, IonIcon, IonItem, IonList, IonInput, IonSelectOption, IonButton, IonModal, IonSelect, useIonToast, IonTextarea } from '@ionic/react';
 import { useParams } from 'react-router';
 import axios from 'axios';
 import './Page.css';
 import useAuth from '../hooks/useAuth'; // Import the custom hook
 import { add, closeOutline } from 'ionicons/icons';
 
+const STATIC_SUBJECT_FAILURE_CASE = [
+  {
+    "ticket_subject_id": "1",
+    "ticket_subject": "Dress required",
+    "ticket_subject_type": "Gaurd",
+    "last_updated_on": "2024-07-10 08:33:05"
+  },
+  {
+    "ticket_subject_id": "2",
+    "ticket_subject": "Need Some more support",
+    "ticket_subject_type": "Gaurd",
+    "last_updated_on": "2024-07-10 08:33:05"
+  },
+  {
+    "ticket_subject_id": "3",
+    "ticket_subject": "Some issue at my end",
+    "ticket_subject_type": "Gaurd",
+    "last_updated_on": "2024-07-10 08:33:50"
+  },
+  {
+    "ticket_subject_id": "4",
+    "ticket_subject": "Salary issue",
+    "ticket_subject_type": "Gaurd",
+    "last_updated_on": "2024-07-10 08:33:50"
+  }
+]
+
 const GetRequests: React.FC = () => {
   // useAuth(); // Enforce login requirement
 
   const [requestData, setRequestData] = useState<any>(null);
+  const [subjectList, setSubjectList] = useState<any>(STATIC_SUBJECT_FAILURE_CASE);
   const [loading, setLoading] = useState(true);
   const [showRequestModal, setShowRequestModal] = useState(false);
   const [priority, setPriority] = useState('LOW');
@@ -20,9 +48,11 @@ const GetRequests: React.FC = () => {
 
   const token = localStorage.getItem('token');
   useEffect(() => {
+    getSubjectListAPI();
     const url = "https://guard.ghamasaana.com/guard_new_api/request.php";
     const formData = new FormData();
     formData.append('action', "request_data");
+    formData.append('req_type', "ticket");
     formData.append('token', token);
     formData.append('subject', "");
     formData.append('message', "");
@@ -32,7 +62,7 @@ const GetRequests: React.FC = () => {
       .then(response => {
         if (response.data && response.data.success) {
           setRequestData(response.data.employee_data.request_data);
-          console.log(response.data.employee_data.request_data, token, )
+          console.log(response.data.employee_data.request_data, token,)
         } else {
           console.error('Failed to fetch requests:', response.data);
         }
@@ -44,11 +74,39 @@ const GetRequests: React.FC = () => {
       });
   }, []);
 
+  function getSubjectListAPI() {
+    let URL = "https://guard.ghamasaana.com/guard_new_api/ticket_subject.php";
+    let formData = new FormData();
+    formData.append('action', "ticket_subject");
+    formData.append('token', token);
+
+    axios.post(URL, formData)
+      .then(response => {
+        if (response.data && response.data.success) {
+          setSubjectList(response?.data?.employee_data);
+        } else {
+          present({
+            message: `Error getting subject list from API!`,
+            duration: 2000,
+            position: 'bottom',
+          });
+        }
+      })
+      .catch(error => {
+        present({
+          message: `Error getting subject list from API!`,
+          duration: 2000,
+          position: 'bottom',
+        });
+      });
+
+  }
+
   const { name } = useParams<{ name: string; }>();
 
-  function newTicketNav(){
+  function newTicketNav() {
     console.log("New Ticket CLickec");
-    setReqType('ticket'); 
+    setReqType('ticket');
     setShowRequestModal(true);
   }
 
@@ -61,7 +119,7 @@ const GetRequests: React.FC = () => {
     formData.append('ReqDesc', reqDesc);
     formData.append('reqotherdetail', "");
     console.log(token);
-    console.log("formDATA create----> ", JSON.stringify(formData)); 
+    console.log("formDATA create----> ", JSON.stringify(formData));
     // return false;
 
     axios
@@ -107,60 +165,40 @@ const GetRequests: React.FC = () => {
       </IonHeader>
 
       <IonContent fullscreen>
-      <IonFab horizontal="end" vertical="bottom" slot="fixed">
-        <IonFabButton onClick={()=> newTicketNav()}>
-          <IonIcon icon={add}></IonIcon>
-        </IonFabButton>
-      </IonFab>
+        <IonFab horizontal="end" vertical="bottom" slot="fixed">
+          <IonFabButton onClick={() => newTicketNav()}>
+            <IonIcon icon={add}></IonIcon>
+          </IonFabButton>
+        </IonFab>
         {loading ? (
           <IonLoading isOpen={loading} message={'Loading...'} />
         ) : (
           <>
-     <div className="header_title">
-        <IonTitle className="header_title ion-text-center">Your Ticket Information</IonTitle>
-      </div>
-      <IonCard className='shift-details-card-content'>
-    
-                {requestData ? (
-                  <IonGrid>
-            
-                    {requestData.map((ticket, index) => (
-                        <IonCard className='card' key={index}>
-                    
-                    <div className="shift-details-column">
-                <p><strong>Request Type: </strong>{ticket.ReqType || 'N/A'}</p>
-                <p><strong>Request Date : </strong>{ticket.ReqDatetime || 'N/A'}</p>
-                <p><strong>Request Description : </strong>{ticket.ReqDesc || 'N/A'}</p> 
-                <p><strong>Request ID : </strong>{ticket.ReqID || 'N/A'}</p> 
-                <p><strong>Request Status : </strong>{ticket.ReqStatus || 'N/A'}</p> 
-                <p><strong>Request Action : </strong>{ticket.ReqAction || 'N/A'}</p> 
-                </div>      
-                  
-                        </IonCard>
-                    ))}
-                  </IonGrid>
-                ) : (
-                  <IonLabel><div className='notFound'>
-                    <IonImg src="./assets/imgs/nodata.svg" alt="header" />
-                    No requests found</div></IonLabel>
-                )}
-
-           
-
-            {/* <div className='pagination'>
-            <ul id="border-pagination">
-            <li><a className="" href="#">«</a></li>
-            <li><a href="#">1</a></li>
-            <li><a href="#" className="active">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li><a href="#">6</a></li>
-            <li><a href="#">»</a></li>
-             </ul> 
-             </div> */}
-
-             </IonCard>
+            <div className="header_title">
+              <IonTitle className="header_title ion-text-center">Your Ticket Information</IonTitle>
+            </div>
+            <IonCard className='shift-details-card-content'>
+              {(requestData && requestData.length > 0) ? (
+                <IonGrid>
+                  {requestData.map((ticket, index) => (
+                    <IonCard className='card' key={index}>
+                      <div className="shift-details-column">
+                        <p><strong>Request Type: </strong>{ticket.ReqType || 'N/A'}</p>
+                        <p><strong>Request Date : </strong>{ticket.ReqDatetime || 'N/A'}</p>
+                        <p><strong>Request Description : </strong>{ticket.ReqDesc || 'N/A'}</p>
+                        <p><strong>Request ID : </strong>{ticket.ReqID || 'N/A'}</p>
+                        <p><strong>Request Status : </strong>{ticket.ReqStatus || 'N/A'}</p>
+                        <p><strong>Request Action : </strong>{ticket.ReqAction || 'N/A'}</p>
+                      </div>
+                    </IonCard>
+                  ))}
+                </IonGrid>
+              ) : (
+                <IonLabel><div className='notFound'>
+                  <IonImg src="./assets/imgs/nodata.svg" alt="header" />
+                  No requests found</div></IonLabel>
+              )}
+            </IonCard>
 
             <div className='footer'>
               <IonTitle className='footer ion-text-center'>Helpline | +91 90999 XXXXX</IonTitle>
@@ -169,38 +207,52 @@ const GetRequests: React.FC = () => {
         )}
         {/* Moal code goes below */}
         <IonModal isOpen={showRequestModal} onDidDismiss={() => setShowRequestModal(false)}>
-            <IonHeader>
-              <IonToolbar>
-                <IonTitle>{'Create Ticket'}</IonTitle>
-                <IonButtons slot="end">
-                  <IonButton onClick={() => setShowRequestModal(false)}>
-                    <IonIcon icon={closeOutline} size="large"></IonIcon>
-                  </IonButton>
-                </IonButtons>
-              </IonToolbar>
-            </IonHeader>
-            <IonContent>
-              <IonList>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>{'Create Ticket'}</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => setShowRequestModal(false)}>
+                  <IonIcon icon={closeOutline} size="large"></IonIcon>
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonList>
+              {/* <IonItem> */}
+                {/* <IonLabel position="floating">Subject</IonLabel> */}
+                {/* <IonInput value={reqSubject} onIonInput={e => setReqSubject(e.detail.value!)}></IonInput> */}
+                {/* <div>{JSON.stringify(subjectList)}</div> */}
                 <IonItem>
-                  <IonLabel position="floating">Subject</IonLabel>
-                  <IonInput value={reqSubject} onIonInput={e => setReqSubject(e.detail.value!)}></IonInput>
+                  <IonSelect 
+                    label="Subject" 
+                    placeholder="Select Subject"
+                    onIonChange={(e) => {
+                      console.log(`ionChange fired with value: ${e.detail.value}`);
+                      setReqSubject(e.detail.value);
+                    }}>
+                    {subjectList && subjectList.map((subjectItem: any) => 
+                    <IonSelectOption value={subjectItem.ticket_subject}>{subjectItem.ticket_subject}</IonSelectOption>
+                  )}
+                  </IonSelect>
                 </IonItem>
-                <IonItem>
-                  <IonLabel position="floating">Description</IonLabel>
-                  <IonInput value={reqDesc} onIonInput={e => setReqDesc(e.detail.value!)}></IonInput>
-                </IonItem>
-                  <IonItem>
-                    <IonLabel>Priority</IonLabel>
-                    <IonSelect value={priority} onIonChange={e => setPriority(e.detail.value)}>
-                      <IonSelectOption value="LOW">Low</IonSelectOption>
-                      <IonSelectOption value="MEDIUM">Medium</IonSelectOption>
-                      <IonSelectOption value="HIGH">High</IonSelectOption>
-                    </IonSelect>
-                  </IonItem>
-              </IonList>
-              <IonButton expand="full" onClick={handleCreateRequest}>Create {'Ticket'}</IonButton>
-            </IonContent>
-          </IonModal>
+              {/* </IonItem> */}
+              <IonItem>
+                <IonLabel position="floating">Description</IonLabel>
+                <IonTextarea value={reqDesc} onIonInput={e => setReqDesc(e.detail.value!)}></IonTextarea>
+              </IonItem>
+              {/* <IonItem>
+                <IonLabel>Priority</IonLabel>
+                <IonSelect value={priority} onIonChange={e => setPriority(e.detail.value)}>
+                  <IonSelectOption value="LOW">Low</IonSelectOption>
+                  <IonSelectOption value="MEDIUM">Medium</IonSelectOption>
+                  <IonSelectOption value="HIGH">High</IonSelectOption>
+                </IonSelect>
+              </IonItem> */}
+            </IonList>
+            <IonButton expand="full" onClick={handleCreateRequest}>Create {'Ticket'}</IonButton>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
